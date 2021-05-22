89927beac324cc9e1c5cdaaf4dc88a7b58426a23
SBFAM22 <47464673+SBFAM22@users.noreply.github.com>
2021-05-22 11:56:03 -0400

    Fixed Error handle

--- Rick.Net-Sol/Rick.Net/CanError.cs
+++ Rick.Net-Sol/Rick.Net/CanError.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,7 @@ namespace Rick
         /// <summary>
         /// The error message if any
         /// </summary>
+        [JsonProperty("error")]
         public string ErrorMessage { get; set; } = "";
 
         /// <summary>
